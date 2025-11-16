16acaadca49eb1f70c60b6ec0658461f3689526d
kwung0206 <kgj0199kgj0199@gmail.com>
2025-11-17 00:15:42 +0900

    경로 오류

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
 // 🔥 원래 있던 전역 스타일 import 다시 추가
-import "./scss/app.scss";   // 파일명이 다르면 실제 쓰는 이름으로 바꿔줘
+import "./scss/App.scss";   // 파일명이 다르면 실제 쓰는 이름으로 바꿔줘
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
